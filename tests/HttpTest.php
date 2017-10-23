e1806789c04c5bb63a1b98643422a5b6fd415647
程会勇 <chenghuiyong@chehubao.com>
2017-10-23 17:11:36 +0800

    phpunit test

--- tests/HttpTest.php
+++ tests/HttpTest.php
@@ -14,16 +14,16 @@ class HttpTest extends PHPUnit_Framework_TestCase
                 'md5' => [
                     'driver' => 'md5',
                     'options' => [
-                        'signKey' => 'ac59f0714d937822d990d880a6754fc7'
+                        'signKey' => '06f7aab08aa2431e6dae6a156fc9e0b4'
                     ]
                 ]
             ],
             'cnvex' => [
                 'protocol' => 'HTTP_FORM_JSON',
                 'signType' => 'MD5',
-                'partnerId' => '17083115321700300060',
+                'partnerId' => 'test',
                 'version' => '1.0',
-                'apiHost' => 'http://open.cnvex.cn/gateway.html',
+                'apiHost' => 'http://222.180.209.130:8810/gateway.html',
                 // 'signKey' => '20ad542cba99a9330dce0429dbeec55e',
                 'debug' => true,
                 'notify' => [
@@ -35,7 +35,7 @@ class HttpTest extends PHPUnit_Framework_TestCase
         $http = new Http($manager, new Client(), $app['cnvex']);
         $parameters = [
             'service' => 'queryUser',
-            'outUserId' => '3742fa50-b275-11e7-b28c-372ca50f1589',
+            'outUserId' => 'E55752A1-B364-4C37-9442-E9D6C0CC8422',
             // 'userId' => '17081609495213000019'
         ];
         $this->assertObjectHasAttribute('userInfo', json_decode($http->post($parameters)));
