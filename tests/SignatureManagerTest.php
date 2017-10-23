5ac17c8e136eaedef8ea3e8b00e655497f63ebe4
程会勇 <chenghuiyong@chehubao.com>
2017-10-23 11:51:33 +0800

    phpunit test

--- tests/SignatureManagerTest.php
+++ tests/SignatureManagerTest.php
@@ -7,15 +7,18 @@ class SignatureManagerTest extends PHPUnit_Framework_TestCase
     {
         $app = [
             'config' => [
-                'cnvex.signature.default' => 'md5',
-                'cnvex.signature.md5' => [
-                    'driver' => 'md5',
-                    'options' => [
-                        'signKey' => '111'
+                'signature' => [
+                    'default' => 'md5',
+                    'md5' => [
+                        'driver' => 'md5',
+                        'options' => [
+                            'signKey' => '111'
+                        ]
                     ]
                 ]
-            ],
+            ]
         ];
+        
         $manager = new SignatureManager($app);
         $this->assertEquals('96e79218965eb72c92a549dd5a330112', $manager->signer()->sign('111'));
     }
