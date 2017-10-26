01c186dff61ac7a2ff7f933d8b58fa2e210468c9
程会勇 <chenghuiyong@chehubao.com>
2017-10-26 14:29:47 +0800

    phpunit

--- tests/HttpTest.php
+++ tests/HttpTest.php
@@ -34,63 +34,63 @@ class HttpTest extends TestCase
             ];
     }
 
-    // public function testQiZhangTongCanRequest()
-    // {
-    //     $app = $this->getDefaults();
-    //     $manager = new SignatureManager($app['signature']);
-    //     $http = new Http($manager, new Client(), $app['cnvex']);
-    //     $parameters = [
-    //         'service' => 'queryUser',
-    //         // 'outUserId' => 'E55752A1-B364-4C37-9442-E9D6C0CC8422',
-    //         'userId' => '17092720111513000002'
-    //     ];
-    //     $this->assertObjectHasAttribute('userInfo', $http->post($parameters));
-    // }
-    //
-    // public function testQueryUser()
-    // {
-    //     $app = $this->getDefaults();
-    //     $manager = new SignatureManager($app['signature']);
-    //     $http = new Api($manager, new Client(), $app['cnvex']);
-    //     $res = $http->queryUser('17092720111513000002');
-    //     $this->assertObjectHasAttribute('userId', $res);
-    // }
-    //
-    // public function testQueryUserBalance()
-    // {
-    //     $app = $this->getDefaults();
-    //     $manager = new SignatureManager($app['signature']);
-    //     $http = new Api($manager, new Client(), $app['cnvex']);
-    //     $res = $http->queryUserBalance('17092720111513000002');
-    //     $this->assertObjectHasAttribute('userId', $res);
-    // }
-    //
-    // public function testSendSMS()
-    // {
-    //     $app = $this->getDefaults();
-    //     $manager = new SignatureManager($app['signature']);
-    //     $http = new Api($manager, new Client(), $app['cnvex']);
-    //     $res = $http->sendCaptcha('15390438190');
-    //     $this->assertTrue($res);
-    // }
-    //
-    // public function testRegisterUser()
-    // {
-    //     $app = $this->getDefaults();
-    //     $manager = new SignatureManager($app['signature']);
-    //     $http = new Api($manager, new Client(), $app['cnvex']);
-    //     $res = $http->registerUser('D3665263-7925-4858-A461-E90368437643', 111111, 15390438190, '程会勇', '513701198709184016', '6222024402027814403');
-    //     $this->assertNotNull($res);
-    // }
-    //
-    // public function testqueryTransaction()
-    // {
-    //     $app = $this->getDefaults();
-    //     $manager = new SignatureManager($app['signature']);
-    //     $http = new Api($manager, new Client(), $app['cnvex']);
-    //     $res = $http->queryTransaction('311710242030203990');
-    //     $this->assertObjectHasAttribute('tradeStatus', $res);
-    // }
+    public function testQiZhangTongCanRequest()
+    {
+        $app = $this->getDefaults();
+        $manager = new SignatureManager($app['signature']);
+        $http = new Http($manager, new Client(), $app['cnvex']);
+        $parameters = [
+            'service' => 'queryUser',
+            // 'outUserId' => 'E55752A1-B364-4C37-9442-E9D6C0CC8422',
+            'userId' => '17092720111513000002'
+        ];
+        $this->assertObjectHasAttribute('userInfo', $http->post($parameters));
+    }
+    
+    public function testQueryUser()
+    {
+        $app = $this->getDefaults();
+        $manager = new SignatureManager($app['signature']);
+        $http = new Api($manager, new Client(), $app['cnvex']);
+        $res = $http->queryUser('17092720111513000002');
+        $this->assertObjectHasAttribute('userId', $res);
+    }
+    
+    public function testQueryUserBalance()
+    {
+        $app = $this->getDefaults();
+        $manager = new SignatureManager($app['signature']);
+        $http = new Api($manager, new Client(), $app['cnvex']);
+        $res = $http->queryUserBalance('17092720111513000002');
+        $this->assertObjectHasAttribute('userId', $res);
+    }
+    
+    public function testSendSMS()
+    {
+        $app = $this->getDefaults();
+        $manager = new SignatureManager($app['signature']);
+        $http = new Api($manager, new Client(), $app['cnvex']);
+        $res = $http->sendCaptcha('15390438190');
+        $this->assertTrue($res);
+    }
+    
+    public function testRegisterUser()
+    {
+        $app = $this->getDefaults();
+        $manager = new SignatureManager($app['signature']);
+        $http = new Api($manager, new Client(), $app['cnvex']);
+        $res = $http->registerUser('D3665263-7925-4858-A461-E90368437643', 111111, 15390438190, '程会勇', '513701198709184016', '6222024402027814403');
+        $this->assertNotNull($res);
+    }
+    
+    public function testqueryTransaction()
+    {
+        $app = $this->getDefaults();
+        $manager = new SignatureManager($app['signature']);
+        $http = new Api($manager, new Client(), $app['cnvex']);
+        $res = $http->queryTransaction('311710242030203990');
+        $this->assertObjectHasAttribute('tradeStatus', $res);
+    }
 
     public function testRedirectWallet()
     {
